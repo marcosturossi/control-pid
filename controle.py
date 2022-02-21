360727f77515ec87bd2d06d905028d7760d2897e
Marcos Turossi <marcosturossi@gmail.com>
2022-02-20 22:14:29 -0300

    Arrumando problema

--- controle.py
+++ controle.py
@@ -6,28 +6,33 @@ import matplotlib.pyplot as plt
 # Modelo Matemático (Sistema Massa Mola Amortecedor)
 # TF (1/ms^2 + bs + k)
 
-m = 100  # massa
-b = 20  # Conficiente de Amortecimento
+m = 280  # massa
+b = 30 # Conficiente de Amortecimento
 k = 17  # Constante da Mola
 
 sys = (ctl.tf([1], [m, b, k]))  # Systema em Malha Aberta
 
 # PID Usar Ziegle Nichols para projetar o controlador
 kp = 5  # Ganho Proporcional
-kd = 5  # Ganho Derivativo
-ki = 10  # Ganho Integral
+kd = 0.5 # Ganho Derivativo
+ki = 1  # Ganho Integral
 
 
 def pid(kp, ki, kd):
     """Retorna a função de transferência do controlador """
-    return ctl.tf([kd, kp, ki], [1])
+    return ctl.tf([kd, kp, ki], [1,0])
 
 
 sys_c = ctl.series(pid(kp, ki, kd), sys)  # Colocando o Controlador PID em serie com a planta
-sys_mf = ctl.feedback(sys_c, 1)  # Fechando a Malha
+sys_mf = ctl.feedback(sys*pid(kp, ki, kd), 1)  # Fechando a Malha
 
-x, y = ctl.step_response(sys_mf)
+print(pid(kp,ki,kd))
+x, y = ctl.step_response(sys)
+x1, y1 = ctl.step_response(sys_mf)
 plt.plot(x, y)
+plt.plot(x1,y1, color='red')
 plt.show()
+
+print(sys_mf)
 
 
